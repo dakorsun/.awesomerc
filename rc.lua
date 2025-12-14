1ff9882bd42e6cf36f32d77f121a826b65de7bdb
dk <dakorsun@gmail.com>
2025-12-14 03:17:45 +0200

    zenburn

--- rc.lua
+++ rc.lua
@@ -22,7 +22,7 @@ require("main.error-handling")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
 beautiful.wallpaper = RC.vars.wallpaper
 -- }}}
 
