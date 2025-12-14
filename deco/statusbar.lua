3c7ff0c2fb3b6af8def311d800e7700e684878eb
dk <dakorsun@gmail.com>
2025-12-14 03:09:43 +0200

    returned batterywidget

--- deco/statusbar.lua
+++ deco/statusbar.lua
@@ -20,6 +20,35 @@ local _M = {}
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 -- {{{ Wibar
+
+-- Text battery widget
+batterywidget = wibox.widget.textbox()
+
+-- Function to update battery info
+local function update_battery_info()
+    awful.spawn.easy_async_with_shell("acpi -b", function(stdout)
+        local perc = stdout:match("(%d?%d?%d)%%")
+        local status = stdout:match(": (%a+),")
+
+        if perc == nil then
+            batterywidget.text = "N/A"
+            return
+        end
+
+        local icon = ""
+        if status == "Charging" then icon = "+ " end
+
+        batterywidget.text = string.format("%s%s%%", icon, perc)
+    end)
+end
+
+local batt_timer = timer({ timeout = 10 })
+batt_timer:connect_signal("timeout", update_battery_info)
+batt_timer:start()
+
+
+update_battery_info()
+
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
@@ -69,6 +98,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytasklist, -- Middle widget
     { -- Right widgets
       layout = wibox.layout.fixed.horizontal,
+      batterywidget,
       mykeyboardlayout,
       wibox.widget.systray(),
       mytextclock,
